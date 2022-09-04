b2df6e710a1841b017d1235581a9ac24fe3f15da
Petrelle <crisis306@hotmail.com>
2022-09-04 18:31:39 +0000

    Add css to nav, add new image

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -43,5 +45,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-
-# Create your models here.
